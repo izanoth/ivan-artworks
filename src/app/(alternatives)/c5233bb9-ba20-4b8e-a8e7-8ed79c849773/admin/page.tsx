c4c2a234c57c31ec25b9804a8e351e12974c9d24
Ivan Cilento <ivanzanoth@gmail.com>
2025-08-27 11:42:39 -0300

    developing admin area

--- src/app/(alternatives)/c5233bb9-ba20-4b8e-a8e7-8ed79c849773/admin/page.tsx
+++ src/app/(alternatives)/c5233bb9-ba20-4b8e-a8e7-8ed79c849773/admin/page.tsx
@@ -1,35 +1,6 @@
-'use client';
-
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-
-type Comment = {
-  name: string;
-  message: string;
-  timestamp: string;
-};
+import { LogoutButton } from './LogoutButton';
 
 export default function AdminPage() {
-  const [comments, setComments] = useState<Comment[]>([]);
-  const router = useRouter();
-
-  async function handleLogout() {
-    try {
-      const res = await fetch('/api/admin/logout', {
-        method: 'POST',
-        credentials: 'include', // garante envio/recebimento de cookies
-      });
-
-      if (res.ok) {
-        router.push('/c5233bb9-ba20-4b8e-a8e7-8ed79c849773'); // ou onde quiser redirecionar
-      } else {
-        console.error('Erro ao fazer logout');
-      }
-    } catch (err) {
-      console.error('Erro de rede no logout', err);
-    }
-  }
-
   return (
     <main className="p-8">
       <h1 className="text-2xl font-bold">Painel de Comentários (Tempo Real)</h1>
@@ -43,13 +14,9 @@ export default function AdminPage() {
       </ul>
       <ul>
       
-        <button
-          type="button"
-          onClick={handleLogout}
-          className="bg-red-500 text-white px-4 py-2 rounded"
-        >
-          Logout
-        </button>
+	      <li>
+	      	<LogoutButton />
+			</li>      
       </ul>
     </main>
   );
